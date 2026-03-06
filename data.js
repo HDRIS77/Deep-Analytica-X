d296197e8ef613a9a59a607f727cf25a2711bd16
HDRIS77 <hatem93g@gmail.com>
2026-03-06 03:50:28 +0200

    Add events data for military activities

---
+++ data.js
@@ -0,0 +1,35 @@
+const events = [
+
+{
+lat:24.7,
+lng:46.7,
+size:0.6,
+color:"yellow",
+title:"Missile Launch"
+},
+
+{
+lat:31.5,
+lng:34.4,
+size:0.8,
+color:"red",
+title:"Military Activity"
+},
+
+{
+lat:35.6,
+lng:51.4,
+size:0.7,
+color:"orange",
+title:"Drone Detected"
+},
+
+{
+lat:55.7,
+lng:37.6,
+size:0.9,
+color:"red",
+title:"Airbase Alert"
+}
+
+]
